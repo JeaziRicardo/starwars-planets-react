5d678e7954b754abb44efbc35ba43be16e4f19de
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-06-09 10:20:27 -0300

    [01] - Renderiza os dados dos planetas no corpo da tabela

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
 
 function Table() {
   const tableHeader = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter',
-    'Climate', 'Gravity', 'Terrain', 'Surface Water', 'Population', 'Films', 'Created'];
+    'Climate', 'Gravity', 'Terrain', 'Surface Water', 'Population', 'Films',
+    'Created', 'Edited', 'URL'];
+  const { data } = useContext(Context);
   return (
     <main>
       <table>
@@ -15,6 +18,25 @@ function Table() {
             )) }
           </tr>
         </thead>
+        <tbody>
+          { data.map((planet) => (
+            <tr key={ planet.name }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films }</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          )) }
+        </tbody>
       </table>
     </main>
   );
