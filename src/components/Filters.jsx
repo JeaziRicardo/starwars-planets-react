3a9e14285eea3fb84d098493f4466fbd4118964c
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-06-13 20:32:40 -0300

    [03] - Cria os inputs para filtrar os planetas por dados númericos

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -16,6 +16,46 @@ function Filters() {
         data-testid="name-filter"
         onChange={ handleChanges }
       />
+      <label htmlFor="column">
+        Colunm
+        <select
+          data-testid="column-filter"
+          name="column"
+          id="column"
+        >
+          <option value="population">Population</option>
+          <option value="orbital_period">Orbital Period</option>
+          <option value="diameter">Diameter</option>
+          <option value="rotation_period">Rotation Period</option>
+          <option value="surface_water">Surface Water</option>
+        </select>
+      </label>
+      <label htmlFor="comparison">
+        Operator
+        <select
+          data-testid="comparison-filter"
+          name="comparison"
+          id="comparison"
+        >
+          <option value="maior que">Maior que</option>
+          <option value="menor que">Menor que</option>
+          <option value="igual a">Igual a</option>
+        </select>
+      </label>
+      <label htmlFor="value-filter">
+        <input
+          data-testid="value-filter"
+          type="number"
+          name="value-filter"
+          id="value-filter"
+        />
+      </label>
+      <button
+        data-testid="button-filter"
+        type="button"
+      >
+        Filtrar
+      </button>
     </section>
   );
 }
