4b38945329a8d5ab39c5ee6b33603faf1824e596
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-06-16 21:28:34 -0300

    [07] - Colocar um data-testid no nome dos planetas

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -21,7 +21,7 @@ function Table() {
         <tbody>
           { filter.map((planet) => (
             <tr key={ planet.name }>
-              <td>{planet.name}</td>
+              <td data-testid="planet-name">{planet.name}</td>
               <td>{planet.rotation_period}</td>
               <td>{planet.orbital_period}</td>
               <td>{planet.diameter}</td>
